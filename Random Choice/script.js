3e7481f4b8c76724559b51ceca9e14c87b651a56
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-03-01 16:23:26 +0530

    completed

--- Random Choice/script.js
+++ Random Choice/script.js
@@ -9,7 +9,13 @@ textarea.focus()   //puts the cursor in this element
 textarea.addEventListener('keyup',(typed)=>{  //whenever a key is left after pressed the function is fired
     createTags(typed.target.value)   
     //console.log(typed.target)   //<textarea id="textarea" placeholder="Enter Your Choices">
-    //console.log(typed.target.value) //whatever is written  
+    //console.log(typed.target.value) //whatever is written
+    if(typed.key==='Enter'){
+        setTimeout(()=>{
+            typed.target.value=''  //clears the text area
+        },10)
+        randomSelect()
+    }  
 })
 
 
@@ -27,4 +33,43 @@ function createTags(input){
         tagEL.innerText = tag   //makes the inner text as the word typed
         tagsel.appendChild(tagEL)   //adds the span to tagsel
     });
+}
+
+
+function randomSelect(){
+    const times = 30
+
+    const interval =setInterval(()=>{
+        const randomTag= pick()   //picks a random tag
+        highlightTag(randomTag)    //highlights it
+        setTimeout(()=>{
+            unhighlightTag(randomTag)   //unhighlights it after 100ms
+        },100)
+    },100)   //all of this repeats after 100ms
+
+    setTimeout(()=>{
+        clearInterval(interval)
+
+        setTimeout(()=>{
+            const randomTag= pick()
+            highlightTag(randomTag)
+            const result = randomTag.innerText
+            const display= document.querySelector('.result')
+            display.innerHTML = `The Computer Generated Choice is ${result}!`
+        },100)
+    },times*100 )
+}
+
+
+
+function pick(){
+    const tags = document.querySelectorAll(".tag")
+    return tags[Math.floor(Math.random()*tags.length)]
+}
+
+function highlightTag(tag){   
+    tag.classList.add('highlight')
+}
+function unhighlightTag(tag){
+    tag.classList.remove('highlight')
 }
