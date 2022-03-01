b92f5d07532a1d417a67719c58b341d9750f5571
sumant7 <91867296+sumant7@users.noreply.github.com>
2022-03-01 13:57:52 +0530

    made the words into array and made the chhoice tags

--- Random Choice/script.js
+++ Random Choice/script.js
@@ -15,5 +15,16 @@ textarea.addEventListener('keyup',(typed)=>{  //whenever a key is left after pre
 
 
 function createTags(input){
-    
+    //const tags= input.split(',')  //splits input by ',' and makes ann array with splitted elements of input
+    const tags=input.split(',').filter(tag=> tag.trim()!=='').map(tag=>tag.trim())  //if tag.trim() does not make it empty atring then do tag.trim()
+
+    tagsel.innerHTML=""  //to stop repeating pileup
+
+
+    tags.forEach(tag=> {
+        const tagEL= document.createElement('span') //creates a span 
+        tagEL.classList.add('tag')  //gives the span class og .tag
+        tagEL.innerText = tag   //makes the inner text as the word typed
+        tagsel.appendChild(tagEL)   //adds the span to tagsel
+    });
 }
